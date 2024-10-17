d333cec81253b2be98990260b9ab0fbe24f15e72
sbdjay <piusd@shernet.sheridancollege.ca>
2024-10-17 13:41:03 -0400

    Final Commit

--- MidtermDanielPius/ComputedProperties.swift
+++ MidtermDanielPius/ComputedProperties.swift
@@ -4,14 +4,23 @@
 //
 //  Created by Daniel Pius on 2024-10-17.
 //
-
+//TITLE PAGE
+//DANIEL PIUS - 991675608
 import Foundation
 
-//var orderClass = Order()
+//var orderClass = Orderlist()
+//
+//func basePriceDark() -> Double{
+//}
 
-//func basePrice() -> Double{
-//    
-//retru}
+//func basePriceOG() -> Double{
+//
+//}
+
+//func basePriceVanilla() -> Double{
+//
+//}
+
 //
 //func Tip() -> Double{
 //    
