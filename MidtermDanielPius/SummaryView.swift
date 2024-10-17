3d36523e3faf2b76c287b3d413e89aec786069f8
sbdjay <piusd@shernet.sheridancollege.ca>
2024-10-17 12:00:10 -0400

    First Comit 15min

---
+++ MidtermDanielPius/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  MidtermDanielPius
+//
+//  Created by Daniel Pius on 2024-10-17.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
