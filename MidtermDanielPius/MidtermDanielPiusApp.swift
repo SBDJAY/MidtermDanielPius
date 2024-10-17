70f20081232033f335716606e0e452395a5155cb
sbdjay <piusd@shernet.sheridancollege.ca>
2024-10-17 11:41:51 -0400

    Initial Commit

---
+++ MidtermDanielPius/MidtermDanielPiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermDanielPiusApp.swift
+//  MidtermDanielPius
+//
+//  Created by Daniel Pius on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct MidtermDanielPiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
